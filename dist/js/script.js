74173ef4f504935555594a76c0a340adbc066fdb
Cujbă George Valentin <george.cujba15@gmail.com>
2022-07-29 00:39:44 +0300

    Add the menu overlays on hover

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,31 @@
 window.addEventListener("load", () => {
 
     let overlayFeatures = document.querySelector(".overlay-features");
-    let overlayFeaturesItems = document.querySelectorAll(".overlay-features_item");
+    let overlayCompany = document.querySelector(".overlay-company");
+
     let featuresMenuItem = document.getElementById("features-menu-item");
+    let companyMenuItem = document.getElementById("company-menu-item");
+    
+    showOverlay(featuresMenuItem, overlayFeatures, "mouseover", "show-overlay");
+    showOverlay(overlayFeatures, overlayFeatures, "mouseover", "show-overlay");
 
-    for(let i = 0; i < overlayFeaturesItems.clientHeight; i++) {
-        overlayFeaturesItems[i].addEventListener("mouseover", () => {
-            overlayFeatures.classList.add("show-overlay");
-        })
-
-        overlayFeaturesItems[i].addEventListener("mouseout", () => {
-            overlayFeatures.classList.remove("show-overlay");
-        })
-    }
-
-    featuresMenuItem.addEventListener("mouseover", () => {
-        overlayFeatures.classList.add("show-overlay");
-    })
-
-    overlayFeatures.addEventListener("mouseleave", () => {
-        overlayFeatures.classList.remove("show-overlay");
-    }) 
+    showOverlay(companyMenuItem, overlayCompany, "mouseover", "show-overlay");
+    showOverlay(overlayCompany, overlayCompany, "mouseover", "show-overlay");
+    
+    hideOverlay(overlayFeatures, overlayFeatures, "mouseleave", "show-overlay");
+    hideOverlay(overlayCompany, overlayCompany, "mouseleave", "show-overlay");
 
 })
+
+function showOverlay (trigger, element, eventName, classToTackle) {
+    trigger.addEventListener(eventName, () => {
+        element.classList.add(classToTackle);
+    })
+}
+
+function hideOverlay (trigger, element, eventName, classToTackle) {
+    trigger.addEventListener(eventName, () => {
+        if(element.classList.contains(classToTackle))
+            element.classList.remove(classToTackle);
+    })
+}
