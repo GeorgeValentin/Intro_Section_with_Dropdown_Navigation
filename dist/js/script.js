2df0705c577f07478f4b0ed4b91e8b56efd000bf
Cujbă George Valentin <george.cujba15@gmail.com>
2022-07-30 00:49:51 +0300

    Add the mobile menu overlay with the animations
    NEXT:
    1. Populate the mobile overlay
    2. Add the content animations

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,6 +6,11 @@ window.addEventListener("load", () => {
     let featuresMenuItem = document.getElementById("features-menu-item");
     let companyMenuItem = document.getElementById("company-menu-item");
     
+    let mobileNavBtn = document.querySelector(".nav-menu_mobile");
+    let mobileCloseBtn = document.querySelector(".nav-menu_closeBtn");
+    let mobileOverlay = document.querySelector(".mobile-overlay");
+    let navLogo = document.querySelector(".nav-menu_logo");
+
     showOverlay(featuresMenuItem, overlayFeatures, "mouseover", "show-overlay");
     showOverlay(overlayFeatures, overlayFeatures, "mouseover", "show-overlay");
 
@@ -14,6 +19,20 @@ window.addEventListener("load", () => {
     
     hideOverlay(overlayFeatures, overlayFeatures, "mouseleave", "show-overlay");
     hideOverlay(overlayCompany, overlayCompany, "mouseleave", "show-overlay");
+
+    mobileNavBtn.addEventListener("click", () => {
+        mobileOverlay.classList.add("show-mobile-overlay");
+        mobileNavBtn.classList.add("hide-mobile-menu-btn");  
+        mobileCloseBtn.classList.add("show-closeBtn");
+        navLogo.classList.add("adjust-page-title-position");
+    })
+
+    mobileCloseBtn.addEventListener("click", () => {
+        mobileOverlay.classList.remove("show-mobile-overlay");
+        mobileCloseBtn.classList.remove("show-closeBtn");
+        mobileNavBtn.classList.remove("hide-mobile-menu-btn");
+        mobileNavBtn.classList.add("adjust-mobile-btn-after-overlay");
+    })
 
 })
 
