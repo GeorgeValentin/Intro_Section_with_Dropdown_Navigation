a6f7ce9789f98ee114032421c7c523d7c354aa76
Cujbă George Valentin <george.cujba15@gmail.com>
2022-07-31 00:43:17 +0300

    Refactor the code

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,21 @@
 window.addEventListener("load", () => {
 
-    let overlayFeatures = document.querySelector(".overlay-features");
-    let overlayCompany = document.querySelector(".overlay-company");
+    const overlayFeatures = document.querySelector(".overlay-features");
+    const overlayCompany = document.querySelector(".overlay-company");
 
-    let featuresMenuItem = document.getElementById("features-menu-item");
-    let companyMenuItem = document.getElementById("company-menu-item");
+    const featuresMenuItem = document.getElementById("features-menu-item");
+    const companyMenuItem = document.getElementById("company-menu-item");
 
-    let mobileNavBtn = document.querySelector(".nav-menu_mobile");
-    let mobileCloseBtn = document.querySelector(".nav-menu_closeBtn");
-    let mobileOverlay = document.querySelector(".mobile-overlay");
-    let navLogo = document.querySelector(".nav-menu_logo");
+    const mobileNavBtn = document.querySelector(".nav-menu_mobile");
+    const mobileOverlay = document.querySelector(".mobile-overlay");
+    const navLogo = document.querySelector(".nav-menu_logo");
 
-    let mobileFeaturesMenuBtn = document.querySelector(".features-container");
-    let mobileFeaturesContainer = document.querySelector(".features-container_content");
-    let isFeatureSelected = false;
-    let mobileAccountContainer = document.querySelector(".nav-menu-mobile_account");
+    const mobileFeaturesMenuBtn = document.querySelector(".features-container");
+    const mobileFeaturesContainer = document.querySelector(".features-container_content");
+    const mobileAccountContainer = document.querySelector(".nav-menu-mobile_account");
 
-    let mobileCompanyMenuBtn = document.querySelector(".company-container");
-    let mobileCompanyContainer = document.querySelector(".nav-menu-mobile_company_content");
-    let isCompanyBtnClicked = false;
+    const mobileCompanyMenuBtn = document.querySelector(".company-container");
+    const mobileCompanyContainer = document.querySelector(".nav-menu-mobile_company_content");
 
     showOverlay(featuresMenuItem, overlayFeatures, "mouseover", "show-overlay");
     showOverlay(overlayFeatures, overlayFeatures, "mouseover", "show-overlay");
@@ -30,17 +27,9 @@ window.addEventListener("load", () => {
     hideOverlay(overlayCompany, overlayCompany, "mouseleave", "show-overlay");
 
     mobileNavBtn.addEventListener("click", () => {
-        mobileOverlay.classList.add("show-mobile-overlay");
-        mobileNavBtn.classList.add("hide-mobile-menu-btn");
-        mobileCloseBtn.classList.add("show-closeBtn");
-        navLogo.classList.add("adjust-page-title-position");
-    })
-
-    mobileCloseBtn.addEventListener("click", () => {
-        mobileOverlay.classList.remove("show-mobile-overlay");
-        mobileCloseBtn.classList.remove("show-closeBtn");
-        mobileNavBtn.classList.remove("hide-mobile-menu-btn");
-        mobileNavBtn.classList.add("adjust-mobile-btn-after-overlay");
+        mobileOverlay.classList.toggle("show-mobile-overlay");
+        mobileNavBtn.classList.toggle("update-menu-btn-image");
+        navLogo.classList.toggle("adjust-page-title-position");
     })
 
     toggleMobileFeatures(
@@ -49,7 +38,6 @@ window.addEventListener("load", () => {
         mobileFeaturesContainer,
         "show-mobile-content",
         "click",
-        isFeatureSelected,
         mobileAccountContainer,
         "features-showcase-account-position"
     );
@@ -60,7 +48,6 @@ window.addEventListener("load", () => {
         mobileCompanyContainer,
         "show-mobile-content",
         "click",
-        isCompanyBtnClicked,
         mobileAccountContainer,
         "company-showcase-account-position"
     );
@@ -68,46 +55,29 @@ window.addEventListener("load", () => {
 })
 
 function toggleMobileFeatures(
-    toggler, 
-    togglerStatusClass, 
-    element, 
-    elementShowClass, 
-    eventName, 
-    isSelected,
+    toggler,
+    togglerStatusClass,
+    element,
+    elementShowClass,
+    eventName,
     accountContainer,
     featuresDroppedDownStyle) {
-    
+
     toggler.addEventListener(eventName, () => {
 
-        if (isSelected === false) {
-            element.classList.add(elementShowClass);
-            toggler.classList.add(togglerStatusClass);
-            isSelected = true;
-            accountContainer.classList.add(featuresDroppedDownStyle);
+        element.classList.toggle(elementShowClass);
+        toggler.classList.toggle(togglerStatusClass);
+        accountContainer.classList.toggle(featuresDroppedDownStyle);
 
-            if(
-                !accountContainer.classList.contains("features-showcase-account-position") ||
-                !accountContainer.classList.contains("company-showcase-account-position")
-                ) 
-            {
-                accountContainer.classList.remove("showcase-account-position");
-            } else {
-                accountContainer.classList.remove("features-showcase-account-position");
-                accountContainer.classList.remove("company-showcase-account-position");
-                accountContainer.classList.add("showcase-account-position");
-            }
-            
-        } else {
-            element.classList.remove(elementShowClass);
-            toggler.classList.remove(togglerStatusClass);
-            isSelected = false;
-            accountContainer.classList.remove(featuresDroppedDownStyle);
-
+        if (
+            accountContainer.classList.contains("features-showcase-account-position") &&
+            accountContainer.classList.contains("company-showcase-account-position")
+        ) {
+            accountContainer.classList.toggle("showcase-account-position");
+        } else if (accountContainer.classList.contains("features-showcase-account-position")) {
+            accountContainer.classList.remove("showcase-account-position");
+            accountContainer.classList.remove("company-showcase-account-position");
         }
-
-        console.log(toggler.classList)
-
-
     })
 }
 
